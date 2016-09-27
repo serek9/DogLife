c4b5c48c708fb1af0621060a1c16a87fcdb490a5
DAM <sergiodiaz808@gmail.com>
2016-09-27 19:34:49 +0200

    Initial Commit

---
+++ DogLife/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DogLife
+//
+//  Created by DAM on 27/9/16.
+//  Copyright © 2016 DAM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
